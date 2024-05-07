78433bedbdce69bd7e555f4e34c898376d816b45
Zack Norton <160171391+grizztyrsen@users.noreply.github.com>
2024-05-07 16:00:02 -0500

    creates grids and makes background color changeto black

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const createGrid = (amtOfGrids) => {
     const row = document.createElement('div');
     row.classList.add('grid-row')
     for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 960/sizeOfGrid
       const gridBox = document.createElement('div')
       gridBox.classList.add('grid-box')
+      gridBox.style.width= `${widthAndHeight}px`
+      gridBox.style.height= `${widthAndHeight}px`
+      gridBox.addEventListener(`mouseenter`, () => {
+        gridBox.style.backgroundColor = `black`
+      })
       row.appendChild(gridBox)
     }
     container.appendChild(row)
