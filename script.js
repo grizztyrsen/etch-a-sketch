b74d5f4f80dda364fea88b4643253cf69660c450
Zack Norton <160171391+grizztyrsen@users.noreply.github.com>
2024-05-07 22:41:03 -0500

    restructured to store color picked on first hover, and increase opacity by .1 on each additional hover, until opacity = 1.0

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
+
 const container = document.querySelector('.container')
 const resetButton = document.querySelector(`button`)
 
-function changeColor() {
-  let randomColor = Math.floor(Math.random()*16777215).toString(16)
-  let rgbValue = "#" + randomColor
-  return rgbValue
+
+//calculates and returns 3 values representing the respective rgb values between 0 and 256
+function createRandomRGB() {
+  const r = Math.floor(Math.random()*256)
+  const g = Math.floor(Math.random()*256)
+  const b = Math.floor(Math.random()*256)
+  return {r, g, b}
 }
 
-const createGrid = (amtOfGrids) => {
+
+//creates application structure, formats gridsquares, and launches event listener
+function createGrid(amtOfGrids){
+
   const wrapper = document.createElement('div')
   wrapper.classList.add('wrapper')
+
+  //creates rows based on prompt input
   for(let i=0; i < amtOfGrids; i++) {
     const row = document.createElement('div');
     row.classList.add('grid-row')
+
+    // creates grid boxes based on prompt input
     for (let j = 0; j < amtOfGrids; j++) {
       const widthAndHeight = 600/amtOfGrids
       const gridBox = document.createElement('div')
       gridBox.classList.add('grid-box')
       gridBox.style.width= `${widthAndHeight}px`
       gridBox.style.height= `${widthAndHeight}px`
-      gridBox.addEventListener(`mouseenter`, () => {
-        gridBox.style.backgroundColor = changeColor()
+
+      // stored outside of event listener so color remains the same when opacity increases
+      const {r, g, b} = createRandomRGB() 
+
+      //sets background color to randomized RGB on 1st hover and increases opacity on concurrent hovers
+      gridBox.addEventListener(`mouseenter`,() => {
+        const currentOpacity = Number(gridBox.style.opacity)
+        const bgColor = `rgb(${r}, ${g}, ${b})`
+        gridBox.style.background = bgColor
+        if(currentOpacity < 1){gridBox.style.opacity = currentOpacity + 0.1}
       })
-      row.appendChild(gridBox)
+
+    row.appendChild(gridBox)
     }
-    wrapper.appendChild(row)
+
+  wrapper.appendChild(row)
   }
-  container.appendChild(wrapper)
+
+container.appendChild(wrapper)
 }
 
-createGrid()
-resetButton.addEventListener('click', () => {
-  let userSize = Number(prompt("Enter desired number of grid squares for each side."))
+// runs the primary function in the application with a default of 16x16 grid
+createGrid(16)
 
-  while (userSize > 100) {
-    userSize = Number(prompt("Please choose a number of 100 or less."))
+
+// receives input from user to determine grid size, prevents a number greater than 100, and "clears" previous grid
+resetButton.addEventListener('click', () => {
+  let gridSize = Number(prompt("Enter desired number of grid squares for each side."))
+
+  while (gridSize > 100) {
+    gridSize = Number(prompt("Please choose a number of 100 or less."))
   }
   const wrapper = document.querySelector(".wrapper")
   wrapper.remove()
-  createGrid(userSize)
+  createGrid(gridSize)
 })
